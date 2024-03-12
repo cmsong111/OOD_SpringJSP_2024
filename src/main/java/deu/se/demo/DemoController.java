248c12261b54dc356069ffe1934eb7b0e2d79117
Namju Kim <cmsong111@naver.com>
2024-03-12 18:09:21 +0900

    feat: Add Index Page

---
+++ src/main/java/deu/se/demo/DemoController.java
@@ -0,0 +1,13 @@
+package deu.se.demo;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class DemoController {
+
+    @GetMapping("/")
+    public String index(){
+        return "project_list";
+    }
+}
